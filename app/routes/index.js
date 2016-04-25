6b1d3133818ed703eaad5d777bd4c1e5929e723b
Adam Ferras <aferras@articulate.com>
2016-04-25 11:30:19 -0600

    Initial commit of working code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,33 +1,19 @@
+/*jslint node: true */
+/*jshint esversion: 6 */
 'use strict';
-
-var path = process.cwd();
 
 module.exports = function (app) {
 
-    app.route('/:query')
-        .get(function(req, res) {
-            var monthNames = [
-              "January", "February", "March",
-              "April", "May", "June", "July",
-              "August", "September", "October",
-              "November", "December"
-            ];
-            var str = req.params.query;
-            var date = new Date(isNaN(str) ? str : parseInt(str, 10));
-            var unixTime = date.getTime()/1000;
-            if(isNaN(date)) {
-                naturalDate = null;
-            } else {
-            var naturalDate = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
-            }
-
-            var dateObj = { 'unix': unixTime, 'natural': naturalDate };
-
-            res.send(JSON.stringify(dateObj));
-        });
-
-    app.route('/')
-        .get(function (req, res) {
-            res.sendFile(path + '/public/index.html');
-        });
+  app.route('/')
+    .get(function(req, res) {
+      var ip = req.socket.remoteAddress; 
+      var language = req.headers['accept-language'].split(',')[0];
+      var operatingSystem = req.headers['user-agent'].split(') ')[0].split(' (')[1];
+      var payload = {
+        ip,
+        language,
+        operatingSystem
+      };
+      res.send(payload);
+    });
 };
