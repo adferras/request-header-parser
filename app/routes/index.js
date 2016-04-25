654c72562b6d1725535ac0f210131922c83e9179
Adam Ferras <aferras@articulate.com>
2016-04-25 11:58:15 -0600

    Fix IP calculation

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
 
   app.route('/')
     .get(function(req, res) {
-      var ip = req.socket.remoteAddress; 
+      var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.connection.remoteAddress || req.socket.connection.remoteAddress; 
       var language = req.headers['accept-language'].split(',')[0];
       var operatingSystem = req.headers['user-agent'].split(') ')[0].split(' (')[1];
       var payload = {
